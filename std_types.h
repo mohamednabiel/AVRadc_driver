e3ea98b8adc320da6cddf99a57d37bc4b6fbf240
mohamednabiel <mohamednabiel717@gmail.com>
2021-04-12 20:42:34 +0200

    .\std_types.h

--- std_types.h
+++ std_types.h
@@ -6,6 +6,7 @@ typedef unsigned int  u32;
 typedef unsigned long long int u64;
 typedef signed char s8;
 typedef signed short int s16;
+typedef signed short int s16;
 typedef signed int s32;
 typedef signed long long int s64;
 typedef float f32;
